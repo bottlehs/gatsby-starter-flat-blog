58dce695c3e4f4797d40648c6b364bcc5af9b226
bottlehs <qudgns9@gmail.com>
2021-03-26 19:05:40 +0900

    피드 화면 graph 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,9 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { eq: false } } }) {
       nodes {
         excerpt
         fields {
