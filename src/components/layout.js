59b49a7919d7f46564e1f66604058d6287751629
bottlehs <qudgns9@gmail.com>
2021-04-21 21:05:39 +0900

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
-import { ThemeToggler } from "gatsby-plugin-dark-mode"
-import Switch from "react-switch"
-import { IconContext } from "react-icons"
-import { RiSunFill, RiMoonClearFill } from "react-icons/ri"
+import React from 'react'
+import { Link } from 'gatsby'
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
+import Switch from 'react-switch'
+import { IconContext } from 'react-icons'
+import { RiSunFill, RiMoonClearFill } from 'react-icons/ri'
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath =
-    location.pathname === rootPath
+  const isRootPath = location.pathname === rootPath
   let header
 
   if (isRootPath) {
@@ -29,24 +28,39 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <header className="global-header">
+        {header}
+        <ul className="navbar">
+          <li>
+            <Link className="" to="/blog">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link className="" to="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </header>
       <div className="theme">
         <ThemeToggler>
           {({ theme, toggleTheme }) => (
             <Switch
-              onChange={e => toggleTheme(theme === "dark" ? "light" : "dark")}
-              checked={theme === "dark"}
+              onChange={(e) => toggleTheme(theme === 'dark' ? 'light' : 'dark')}
+              checked={theme === 'dark'}
               offColor="#b2dcf2"
               onColor="#423090"
               uncheckedIcon={
                 <IconContext.Provider
-                  value={{ color: "#f4da87", className: "theme-sun-icon" }}
+                  value={{ color: '#f4da87', className: 'theme-sun-icon' }}
                 >
                   <RiSunFill />
                 </IconContext.Provider>
               }
               checkedIcon={
                 <IconContext.Provider
-                  value={{ color: "#e7eee5", className: "theme-moon-icon" }}
+                  value={{ color: '#e7eee5', className: 'theme-moon-icon' }}
                 >
                   <RiMoonClearFill />
                 </IconContext.Provider>
@@ -55,21 +69,6 @@ const Layout = ({ location, title, children }) => {
           )}
         </ThemeToggler>
       </div>
-      <header className="global-header">
-        {header}
-        <ul className="navbar">
-          <li>
-            <Link className="" to="/blog">
-              Blog
-            </Link>            
-          </li>          
-          <li>
-            <Link className="" to="/about">
-              About
-            </Link>            
-          </li>
-        </ul>
-      </header>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
